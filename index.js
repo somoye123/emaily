05569dd253cc4b3728b03b2fcd6a08dd56d2bbbe
Somoye <somoye.ayotunde@gmail.com>
2019-04-04 01:22:17 -0800

    changed greetings

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/',(req, res) => {
-  res.send({hi: 'there'});
+app.get("/", (req, res) => {
+  res.send({ bye: "somoye" });
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
